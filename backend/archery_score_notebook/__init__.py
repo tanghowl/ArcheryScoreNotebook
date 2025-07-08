5371dee15092ece6dc12d5f82893e49ce43ee7ce
tanghowl <tanghowl@163.com>
2025-07-08 16:16:35 +0800

    test

--- backend/archery_score_notebook/__init__.py
+++ backend/archery_score_notebook/__init__.py
@@ -9,7 +9,6 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
